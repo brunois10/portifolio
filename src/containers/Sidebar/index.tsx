5bcf92f135d16ae648a78660cfd72c07da5ef94a
Bruno Guimarães <brunogdepaula@hotmail.com>
2023-12-22 20:17:47 -0300

    Finalizando o projeto

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -3,7 +3,11 @@ import Paragrafo from '../../components/Paragrafo'
 import Title from '../../components/Title'
 import { Description, BotaoTema, SidebarContainer } from './style'
 
-const Sidebar = () => (
+type Props = {
+  trocaTema: () => void
+}
+
+const Sidebar = (props: Props) => (
   <aside>
     <SidebarContainer>
       <Avatar />
@@ -14,7 +18,7 @@ const Sidebar = () => (
       <Description tipo="principal" fontSize={12}>
         Engenheiro front-end
       </Description>
-      <BotaoTema>Trocar tema</BotaoTema>
+      <BotaoTema onClick={props.trocaTema}>Trocar tema</BotaoTema>
     </SidebarContainer>
   </aside>
 )
